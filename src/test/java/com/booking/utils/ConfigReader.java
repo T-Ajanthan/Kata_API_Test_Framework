0e55739404393268ebd052e44fc33fffa459c57b
AjanthanT <ajanthan27t@live.com>
2025-08-27 18:36:28 +0200

    Adding ConfigReader file

---
+++ src/test/java/com/booking/utils/ConfigReader.java
@@ -0,0 +1,26 @@
+package com.booking.utils;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    private static Properties properties = new Properties();
+
+    static {
+        try (InputStream input = ConfigReader.class.getClassLoader()
+                .getResourceAsStream("application.properties")) {
+            if (input != null) {
+                properties.load(input);
+            } else {
+                throw new RuntimeException("application.properties not found in classpath");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String get(String key) {
+        return properties.getProperty(key);
+    }
+}
